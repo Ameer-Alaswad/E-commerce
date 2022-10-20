533c86205bb59ad51023757498f61f768987d803
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-10-20 19:11:00 +0200

    displays products and adds display product route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import PrimarySearchAppBar from './components/navbar/Header';
+import DisplayProducts from './components/display-products/DisplayProducts';
+import products from "./assets/data"
 
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import DisplayProduct from './components/display-product/DisplayProduct';
 
 function App() {
   return (
@@ -8,6 +16,12 @@ function App() {
       <header >
         <PrimarySearchAppBar />
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <DisplayProducts products={ products } /> } />
+          <Route path="/product/:id" element={ <DisplayProduct /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
