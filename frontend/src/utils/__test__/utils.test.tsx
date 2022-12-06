2344b06d062c3e1a51d06cfb8ba13b351d21d925
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-12-06 09:51:32 +0100

    creates sign in component

--- frontend/src/utils/__test__/utils.test.tsx
+++ frontend/src/utils/__test__/utils.test.tsx
@@ -1,21 +1,152 @@
-import { addToShoppingCartLogic } from "../utils"
+import { addToShoppingCartLogic, checkUserLoggedIn } from "../utils";
 
-const name = "ameer"
-const lastName = "alaswad"
-const kalb = (name: string, lastName: string) => {
-    if (name.startsWith("A")) {
-        return true
-    }
-    if (name.length > 10) {
-        return false
-    }
-    if (typeof name === "string") {
-        return name + lastName
-    }
+describe("addToShoppingCartLogic", () => {
+    const cartItems = [
+        {
+            productId: "Adidas Fit Pant",
+            quantity: 1,
+            productLimit: 6,
+        },
+    ];
 
-}
-it("3absko", () => {
-    expect(kalb("rrrrrrrrrrrrrrrr", lastName)).toBe(false)
-    expect(kalb(name, lastName)).toBe("ameeralaswad")
-    expect(kalb("Ameer", lastName)).toBeTruthy()
-})
+    const product = [
+        {
+            name: "Nike Slim shirt",
+            label: "nike-slim-shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg", // 679px × 829px
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt",
+        },
+    ];
+
+    const mockSetCartItems = jest.fn();
+
+    test("productIsNotInShoppingCart", () => {
+        const alert = jest.fn();
+        const props = {
+            productName: "Adidas",
+            cartItems,
+            setCartItems: mockSetCartItems,
+            product,
+        };
+
+        addToShoppingCartLogic(props);
+
+        expect(mockSetCartItems).toHaveBeenCalledWith([
+            {
+                productId: "Adidas Fit Pant",
+                quantity: 1,
+                productLimit: 6,
+            },
+            { productId: "Adidas", productLimit: 6, quantity: 1 },
+        ]);
+    });
+
+    test("reachedProductLimitForUser", () => {
+        const cartItems = [
+            {
+                productId: "Adidas Fit Pant",
+                quantity: 1,
+
+                productLimit: 0,
+            },
+        ];
+
+        global.alert = jest.fn().mockImplementation(() => "product per purchase limit")
+        const props = {
+            productName: "Adidas Fit Pant",
+            cartItems,
+            setCartItems: mockSetCartItems,
+            product,
+        };
+        addToShoppingCartLogic(props)
+        expect(global.alert).toHaveBeenCalledTimes(1);
+        expect(global.alert).toReturnWith("product per purchase limit");
+
+    });
+
+    test("productNotInStock", () => {
+        const product = [
+            {
+                name: "Nike Slim shirt",
+                label: "nike-slim-shirt",
+                category: "Shirts",
+                image: "/images/p1.jpg", // 679px × 829px
+                price: 120,
+                countInStock: 0,
+                brand: "Nike",
+                rating: 4.5,
+                numReviews: 10,
+                description: "high quality shirt",
+            },
+        ];
+        const cartItems = [
+            {
+                productId: "Adidas Fit Pant",
+                quantity: 5,
+
+                productLimit: 6,
+            },
+        ];
+
+        global.alert = jest.fn().mockImplementation(() => "error")
+        const props = {
+            productName: "Adidas Fit Pant",
+            cartItems,
+            setCartItems: mockSetCartItems,
+            product,
+        };
+        // expect(addToShoppingCartLogic(props)).toBe()
+        addToShoppingCartLogic(props)
+        // expect(global.alert).toHaveBeenCalledTimes(1);
+        expect(global.alert).toReturnWith("error");
+
+    });
+    test("productInStock", () => {
+        const props = {
+            productName: "Adidas Fit Pant",
+            cartItems,
+            setCartItems: mockSetCartItems,
+            product,
+        };
+        addToShoppingCartLogic(props);
+
+        expect(mockSetCartItems).toHaveBeenCalledWith([
+            { productId: "Adidas Fit Pant", quantity: 2, productLimit: 5 },
+        ]);
+    });
+
+});
+
+
+describe("checkUserLoggedIn", () => {
+    const userObj = {
+        _id: "string",
+        name: "string",
+        email: "string",
+        isAdmin: false,
+        token: "string",
+    };
+
+    const localStorageMock = {
+        getItem: jest.fn(() => userObj),
+    };
+    test("checkUserLoggedIn", () => {
+        global.localStorage = localStorageMock as unknown as Storage;
+        JSON.parse = jest.fn().mockImplementationOnce(() => userObj);
+
+        expect(checkUserLoggedIn(userObj)).toEqual(userObj);
+    });
+
+
+
+
+});
+
+
+
--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -1,51 +1,75 @@
-// Types 
-import { productsType } from "../components/display-products/displayProductsInterface"
-import { product, userSignin } from "../contexts/shopping-cart-context/shoppingCartContextTypes"
+// Types
+import { productsType } from "../components/display-products/displayProductsInterface";
+import {
+    product,
+    userSignin,
+} from "../contexts/shopping-cart-context/shoppingCartContextTypes";
 
-type addToShoppingCartTypes = {
-    productName: string
-    cartItems: product[]
-    setCartItems: (value: product[]) => void
-    product: productsType[]
-}
+export type AddToShoppingCartTypes = {
+    productName: string;
+    cartItems: product[];
+    setCartItems: (value: product[]) => void;
+    product: productsType[];
+};
 
-export const addToShoppingCartLogic = ({ productName, cartItems, setCartItems, product }: addToShoppingCartTypes) => {
-    //    declarations
+export const addToShoppingCartLogic = ({
+    productName,
+    cartItems,
+    setCartItems,
+    product,
+}: AddToShoppingCartTypes) => {
+    const productIsNotInShoppingCart = cartItems.every(
+        (item) => productName !== item.productId
+    );
+    if (productIsNotInShoppingCart) {
+        return setCartItems([
+            ...cartItems,
+            { productId: String(productName), quantity: 1, productLimit: 6 },
+        ]);
+    }
     const handleProductQuantityLimitations = cartItems.forEach((item) => {
 
-        if (item?.productId === productName && item?.productLimit <= 1) {
-            return alert("product per purchase limit")
-        }
+        const reachedProductLimitForUser =
+            item?.productId === productName && item?.productLimit <= 1;
+        if (reachedProductLimitForUser) {
+            console.log("reached limit");
 
-        if (item?.productId === productName && product[0]?.countInStock <= item?.quantity) {
+            return alert("product per purchase limit");
+        }
+        const productInStock =
+            item?.productId === productName &&
+            product[0]?.countInStock >= item?.quantity;
+        if (!productInStock) {
+            console.log("not in stock");
+
             return alert("error")
         }
+        if (productInStock) {
+            console.log("in stock");
 
-        if (item?.productId === productName && product[0]?.countInStock >= item?.quantity) {
-            const changeProductQuantity = cartItems.filter(item => {
+            const changeProductQuantity = cartItems.filter((item) => {
                 if (item?.productId === productName) {
-                    item.quantity += 1
-                    item.productLimit -= 1
-                    return item
+                    item.quantity += 1;
+                    item.productLimit -= 1;
+                    return item;
                 }
-                return item
-            })
-            return setCartItems([...changeProductQuantity])
-        }
-        const productIsNotInShoppingCart = cartItems.every(item => productName !== item.productId)
-        //  here the logic starts 
-        if (productIsNotInShoppingCart) {
-            return setCartItems([...cartItems, { productId: String(productName), quantity: 1, productLimit: 6 }])
-        }
-    })
-    return handleProductQuantityLimitations
+                return item;
+            });
+            console.log(changeProductQuantity);
 
-}
+            return setCartItems([...changeProductQuantity]);
+        }
+
+        //  here the logic starts
+
+    });
+    return handleProductQuantityLimitations;
+};
 
 export const checkUserLoggedIn = (userSignin: userSignin | null) => {
-    const user: any = localStorage.getItem('userData')
-    const parsedUser = JSON.parse(user)
-    let userSigned = userSignin
-    !userSignin ? userSigned = parsedUser : userSigned = userSignin
-    return userSigned
-}
+    const user: any = localStorage.getItem("userData");
+    const parsedUser = JSON.parse(user);
+    let userSigned = userSignin;
+    !userSignin ? (userSigned = parsedUser) : (userSigned = userSignin);
+    return userSigned;
+};
