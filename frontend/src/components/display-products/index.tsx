5583a58605c76bec3b73993c5acc6b2a96cb3e88
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2023-07-18 18:54:08 +0200

    adds PRODUCTS_PATH constant

--- frontend/src/components/display-products/index.tsx
+++ frontend/src/components/display-products/index.tsx
@@ -2,10 +2,11 @@ import useProductsFetch from "../../hooks/useProductsFetch";
 import DisplayProductsList from "./DisplayProductsList";
 import AnimatedLoadingIcon from "../AnimatedLoadingIcon";
 import ProductsNotFoundAlert from "../Error";
+import { PRODUCTS_PATH } from "../constants/path";
 
 const DisplayProducts = () => {
 
-    const { isError, isLoading, data } = useProductsFetch("/api/product");
+    const { isError, isLoading, data } = useProductsFetch(PRODUCTS_PATH);
 
     return (
         <>
